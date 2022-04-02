6aa5456d2727bf308162063084596d426946fad7
Rasalahmad <rasalahmadsit@gmail.com>
2022-04-03 00:13:02 +0600

    Add particle

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -1,42 +1,158 @@
-import { Button, Grid } from '@mui/material';
-import { Box } from '@mui/system';
-import React from 'react';
-import SaveIcon from '@mui/icons-material/Save';
-import resume from '../../../resume.pdf';
-import Typewriter from 'typewriter-effect';
-import './Banner.css';
+import { Button, Grid } from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
+import SaveIcon from "@mui/icons-material/Save";
+import resume from "../../../resume.pdf";
+import Typewriter from "typewriter-effect";
+import "./Banner.css";
+import ProfilePic from "../../../images/my_profile.jpeg";
+import Particles from "react-tsparticles";
 
 const Banner = () => {
-    return (
-        <Box sx={{ width: '80%', m: 'auto' }}>
-            <Grid container spacing={2} sx={{ pt: '100px' }}>
-                <Grid item xs={12} sm={12} md={6}>
-                    <img style={{ width: '300px', borderRadius: '50%' }} src="https://scontent.fcgp7-1.fna.fbcdn.net/v/t1.6435-9/199091597_1194070917685323_5100945657730497712_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeEXKLo75OjQOnzJL9pDFAKfcvaSZI4XOkFy9pJkjhc6QUQMcsVd4kS9RbiWYZ2z8vdaroDp_d4V04wTlloL1NEL&_nc_ohc=Pb0VQaSkiBIAX_AwerI&_nc_ht=scontent.fcgp7-1.fna&oh=2b25c71d3d8c66f58c8db67788c262bd&oe=61CA7770" alt="" />
-                </Grid>
-                <Grid item xs={12} sm={12} md={6}>
-                    <span>My Name Is</span>
-                    <h1>RASAL AHMAD</h1>
-                    <h3>I Am A <p style={{ color: 'deeppink' }}> <Typewriter
-                        options={{
-                            strings: ['Junior Full Stack Developer', 'Graphics Designer'],
-                            autoStart: true,
-                            loop: true,
-                        }}
-                    />
-                    </p>
-                    </h3>
-                    <p>Develop amazing web and app experiences with tips and advice from the experts at Web.Dev. Keep your users engaged with the latest web development capabilities! Website Performance. Website Visibility. Modern Web Techniques. Network Resilience Tips.</p>
-                    <a className="buttons" href={resume} download ><Button variant="contained" startIcon={<SaveIcon />}>
-                        Download CV
-                    </Button></a>
-                    <a className="buttons" href="#contact">
-                        <Button sx={{ mx: '10px', background: 'deeppink' }} variant="contained" startIcon={<SaveIcon />} >
-                            Hire Me
-                        </Button></a>
-                </Grid>
-            </Grid>
-        </Box>
-    );
+  const particlesInit = (main) => {
+    console.log(main);
+
+    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+  };
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
+  return (
+    <div>
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          fpsLimit: 120,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              bubble: {
+                distance: 400,
+                duration: 2,
+                opacity: 0.8,
+                size: 40,
+              },
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#ffffff",
+            },
+            links: {
+              color: "#ffffff",
+              distance: 150,
+              enable: false,
+              opacity: 0.5,
+              width: 1,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outMode: "bounce",
+              random: false,
+              speed: 2,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 800,
+              },
+              value: 120,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              random: true,
+              value: 5,
+            },
+          },
+          detectRetina: true,
+        }}
+      />
+      <Box sx={{ width: "80%", m: "auto" }}>
+        <Grid container spacing={2} sx={{ pt: "100px" }}>
+          <Grid item xs={12} sm={12} md={6}>
+            <img
+              style={{ width: "400px", borderRadius: "50%" }}
+              src={ProfilePic}
+              alt="Profile"
+            />
+          </Grid>
+          <Grid item xs={12} sm={12} md={6} style={{ paddingTop: "50px" }}>
+            <span>My Name Is</span>
+            <h1>RASAL AHMAD</h1>
+            <h3 style={{ display: "flex" }}>
+              I Am
+              <p style={{ color: "deeppink", marginLeft: "5px" }}>
+                {" "}
+                <Typewriter
+                  options={{
+                    strings: [
+                      "Software Engineer",
+                      "Frontend Developer",
+                      "Full Stack Developer",
+                      "React Js Developer",
+                    ],
+                    autoStart: true,
+                    loop: true,
+                  }}
+                />
+              </p>
+            </h3>
+            <p>
+              Develop amazing web and app experiences with tips and advice from
+              the experts at Web.Dev. Keep your users engaged with the latest
+              web development capabilities! Website Performance. Website
+              Visibility. Modern Web Techniques. Network Resilience Tips.
+            </p>
+            <a className="buttons" href={resume} download>
+              <Button variant="contained" startIcon={<SaveIcon />}>
+                Download CV
+              </Button>
+            </a>
+            <a className="buttons" href="#contact">
+              <Button
+                sx={{ mx: "10px", background: "deeppink" }}
+                variant="contained"
+                startIcon={<SaveIcon />}
+              >
+                Hire Me
+              </Button>
+            </a>
+          </Grid>
+        </Grid>
+      </Box>
+    </div>
+  );
 };
 
 export default Banner;
--- src/pages/Home/Footer/Footer.js
+++ src/pages/Home/Footer/Footer.js
@@ -1,75 +1,100 @@
-import React from 'react';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import TextField from '@mui/material/TextField';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+import React from "react";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 
-import './Footer.css'
+import "./Footer.css";
 
 const Footer = () => {
-    return (
-        <Box className='footer-container'>
-            <Grid container spacing={2} className="container">
-                <Grid item xs={6} md={3}>
-                    <Typography variant="h6" sx={{ fontWeight: 'bold', my: '20px' }} component="div">
-                        CONTACT US
-                    </Typography>
-                    <Typography variant="body1" component="div">
-                        <i className="fas fa-phone"></i> ++8016-256-053-40 <br />
-                        <i className="fas fa-map-marker"></i> VIP Road, Bandarban Saadar, Bandarban, Bangladesh <br />
-                        <i className="fas fa-envelope"></i> rasalahmadsit@gmail.com
-                    </Typography>
-                    <Typography variant="body1" component="div">
-                        <i className="fab fa-facebook"></i>
-                        <i className="fab fa-twitter"></i>
-                        <i className="fab fa-linkedin"></i>
-                        <i className="fab fa-google-plus-g"></i>
-                    </Typography>
-                </Grid>
-                <Grid item xs={6} md={3}>
-                    <Typography variant="h6" sx={{ fontWeight: 'bold', my: '20px' }} component="div">
-                        INFORMATION
-                    </Typography>
-                    <Typography variant="body1" component="div">
-                        About Me <br />
-                        Contact Me <br />
-                        FAQ? <br />
-                        News <br />
-                        Advance Search <br />
-                        Sills Map <br />
-                    </Typography>
-                </Grid>
-                <Grid item xs={6} md={3}>
-                    <Typography variant="h6" sx={{ fontWeight: 'bold', my: '20px' }} component="div">
-                        INFORMATION
-                    </Typography>
-                    <Typography variant="body1" component="div">
-                        Meseum <br />
-                        Esperienza <br />
-                        Academia <br />
-                        Blogs <br />
-                        Advance Search <br />
-                        Careers <br />
-                        Mobile App   <br />
-                    </Typography>
-                </Grid>
-                <Grid item xs={6} md={3}>
-                    <Typography variant="h6" sx={{ fontWeight: 'bold', my: '20px' }} component="div">
-                        SUBSCRIBE NEWSLETTER
-                    </Typography>
-                    <Typography variant="body1" component="div">
-                        Enter your email address below to subscribe to my newsletter. Get update surveys and offers
-                    </Typography>
-                    <Box>
-                        <TextField id="standard-basic" sx={{ width: '75%', my: '15px' }} label="Your Email" type="email" variant="standard" />
-                        <Button sx={{ width: '75%', color: '#fff' }} variant="contained">Subscribe</Button>
-                    </Box>
-
-                </Grid>
-            </Grid>
-        </Box>
-    );
+  return (
+    <Box className="footer-container">
+      <Grid container spacing={2} className="container">
+        <Grid item xs={6} md={3}>
+          <Typography
+            variant="h6"
+            sx={{ fontWeight: "bold", my: "20px" }}
+            component="div"
+          >
+            CONTACT US
+          </Typography>
+          <Typography variant="body1" component="div">
+            <i className="fas fa-phone"></i> ++8016-256-053-40 <br />
+            <i className="fas fa-map-marker"></i> VIP Road, Bandarban Saadar,
+            Bandarban, Bangladesh <br />
+            <i className="fas fa-envelope"></i> rasalahmadsit@gmail.com
+          </Typography>
+          <Typography variant="body1" component="div">
+            <i className="fab fa-facebook"></i>
+            <i className="fab fa-twitter"></i>
+            <i className="fab fa-linkedin"></i>
+            <i className="fab fa-google-plus-g"></i>
+          </Typography>
+        </Grid>
+        <Grid item xs={6} md={3}>
+          <Typography
+            variant="h6"
+            sx={{ fontWeight: "bold", my: "20px" }}
+            component="div"
+          >
+            INFORMATION
+          </Typography>
+          <Typography variant="body1" component="div">
+            About Me <br />
+            Contact Me <br />
+            FAQ? <br />
+            News <br />
+            Advance Search <br />
+            Sills Map <br />
+          </Typography>
+        </Grid>
+        <Grid item xs={6} md={3}>
+          <Typography
+            variant="h6"
+            sx={{ fontWeight: "bold", my: "20px" }}
+            component="div"
+          >
+            INFORMATION
+          </Typography>
+          <Typography variant="body1" component="div">
+            Meseum <br />
+            Esperienza <br />
+            Academia <br />
+            Blogs <br />
+            Advance Search <br />
+            Careers <br />
+            Mobile App <br />
+          </Typography>
+        </Grid>
+        <Grid item xs={6} md={3}>
+          <Typography
+            variant="h6"
+            sx={{ fontWeight: "bold", my: "20px" }}
+            component="div"
+          >
+            SUBSCRIBE NEWSLETTER
+          </Typography>
+          <Typography variant="body1" component="div">
+            Enter your email address below to subscribe to my newsletter. Get
+            update surveys and offers
+          </Typography>
+          <Box>
+            <TextField
+              id="standard-basic"
+              sx={{ width: "75%", my: "15px", color: "#ffffff" }}
+              label="Your Email"
+              type="email"
+              variant="standard"
+            />
+            <Button sx={{ width: "75%", color: "#fff" }} variant="contained">
+              Subscribe
+            </Button>
+          </Box>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 };
 
 export default Footer;
